7fe80b92483baa382ec0738a6fa748c97342306b
damienponcet1-ops <damienponcet1@gmail.com>
2026-09-25 14:08:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,47 @@
-const CACHE_NAME = 'moonboard-v51';
+const CACHE_NAME = 'mini-moonboard-v2';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './mini_moonboard_bg.jpg',
+    './manifest.json'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './mini_moonboard_bg.jpg',
-        './icon-192.png'
-      ]);
-    })
-  );
-  self.skipWaiting();
+// Événement d'installation : Mise en cache des ressources
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Mise en cache des ressources...');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  self.clients.claim();
+// Événement d'activation : Nettoyage des anciens caches
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('Nettoyage de l\'ancien cache:', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+    self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+// Événement fetch : Servir depuis le cache en priorité, sinon réseau
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Retourne la version en cache si elle existe, sinon fait la requête réseau
+                return response || fetch(event.request);
+            })
+    );
 });
